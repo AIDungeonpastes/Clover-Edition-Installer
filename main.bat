7f2cebc3a7949b1279800560e3d568791f06682c
AIDungeonpastes <58863583+AIDungeonpastes@users.noreply.github.com>
2019-12-31 13:05:50 -0500

    Add files via upload

--- main.bat
+++ main.bat
@@ -1,10 +1,9 @@
 @echo off
 
 title Clover Edition Installer
-set git=%~dp0mingit\cmd\git.exe
 
 start /wait pip install torch -f https://download.pytorch.org/whl/torch_stable.html
 start /wait pip install transformers pyjarowinkler colorama
-start /wait %git% clone --depth=1 "https://github.com/cloveranon/Clover-Edition/"
-echo And now the last setup step is up to you, you need to torrent a model and place it in the models folder, more information here: https://github.com/cloveranon/Clover-Edition#models
+start /wait git clone --depth=1 "https://github.com/cloveranon/Clover-Edition/"
+echo And now the last step is up to you, you need to torrent a model and place it in the models folder, more information here: https://github.com/cloveranon/Clover-Edition#models
 cmd /k
