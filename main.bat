de0563cc1400af73b8451331665acb1e56b9cc89
AIDungeonpastes <58863583+AIDungeonpastes@users.noreply.github.com>
2019-12-31 17:57:56 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -11,5 +11,6 @@ echo It will take a while to get up to speed.
 echo DHT errors are normal.
 start /wait %aria% --max-connection-per-server 16 --split 64 --bt-max-peers 500 --seed-ratio=1.0 --seed-time=120 --summary-interval=15 --disable-ipv6 --check-integrity --stderr "https://raw.githubusercontent.com/cloveranon/Clover-Edition/master/model.torrent"
 move model_v5_pytorch Clover-Edition\models\pytorch-gpt2-xl-aid2-v5
-echo The setup for Clover Edition has completed successfully, to play the game open the Clover-Edition directory in your terminal of choice (CMD, Powershell, etc...) and type python (or python3) play.py
+echo The setup for Clover Edition has completed successfully, to play the game open the Clover-Edition directory in your terminal of choice and type python (or python3) play.py
+echo For terminal I would recommend cmder (https://cmder.net) or windows terminal (https://github.com/microsoft/terminal/releases) if you use windows 10. 
 cmd /k
